cca509ad0376e39792c103bb42272dbe383adb87
ladymelven <ladymelven@yandex.ru>
2021-07-18 20:23:05 +0300

    rewrite to fp

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,112 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import {
+  compose,
+  allPass,
+  all,
+  gt,
+  gte,
+  lte,
+  length,
+  partialRight,
+  prop,
+  composeWith
+} from 'ramda';
 
- const api = new Api();
- 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
- 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
- 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
- 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
- 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
- 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
- 
+const api = new Api();
+
+const lengthBetween = (string, lower, higher) => {
+  const strLength = length(string);
+  return all(val => val, [gte(strLength, lower), lte(strLength, higher)]);
+}
+const lengthBetweenTwoTen = partialRight(lengthBetween, [2, 10]);
+
+const getNumber = (val) => Number.parseFloat(val);
+const getInteger = compose(Math.round, getNumber);
+
+const square = num => num * num;
+
+const getResidue = (num, divider) => num % divider;
+const getResidueThree = partialRight(getResidue, [3]);
+
+const isPositive = (val) => {
+  const number = getNumber(val);
+  if (number) {
+    return gt(number, 0);
+  }
+  return false;
+}
+
+const isValidNumber = (val) => {
+  return /^\d+\.?\d*$/.test(val);
+}
+
+const validate = allPass([lengthBetweenTwoTen, isPositive, isValidNumber]);
+
+const binaryApi = api.get('https://api.tech/numbers/base');
+const convertToBinary = async (number) => await binaryApi({from: 10, to: 2, number});
+const getAnimal = api.get('https://animals.tech/');
+
+const processSequence = async ({value, writeLog, handleSuccess, handleError}) => {
+  const writeAndPassOn = (val) => {
+    if (val || val === 0) {
+      writeLog(val);
+      return val;
+    }
+  }
+
+  const validateAndPassOn = val => {
+    const isValid = validate(val);
+
+    if (isValid) {
+      return val;
+    } else {
+      handleError('Validation Error');
+    }
+  };
+
+  const composeWithPromise = composeWith(async (fn, prevResult) => {
+    try {
+      const resolved = await prevResult;
+      return fn(resolved);
+    } catch (err) {
+      handleError(err);
+    }
+  });
+
+  composeWithPromise([
+    handleSuccess,
+    prop('result'),
+    getAnimal,
+    writeAndPassOn,
+    getResidueThree,
+    writeAndPassOn,
+    square,
+    writeAndPassOn,
+    length,
+    writeAndPassOn,
+    prop('result'),
+    convertToBinary,
+    writeAndPassOn,
+    getInteger,
+    validateAndPassOn,
+    writeAndPassOn
+  ])(value);
+}
+
  export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,6 +1,6 @@
 /**
  * @file Домашка по FP ч. 1
- * 
+ *
  * Основная задача — написать самому, или найти в FP библиотеках функции anyPass/allPass
  * Эти функции/их аналоги есть и в ramda и в lodash
  *
@@ -13,38 +13,115 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
-// 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+import { SHAPES, COLORS } from '../constants';
+import {
+  allPass,
+  partialRight,
+  propEq,
+  complement,
+  anyPass,
+  prop,
+  keys,
+  equals,
+  gte,
+  lte,
+  all,
+  map,
+  filter,
+  length,
+  max,
+  values,
+  reduce
+} from 'ramda';
 
-    return star === 'red' && square === 'green';
-};
+const shapeIsColor = (prop, color) => propEq(prop, color);
+
+const shapeIsWhite = partialRight(shapeIsColor, [COLORS.WHITE]);
+const shapeIsGreen = partialRight(shapeIsColor, [COLORS.GREEN]);
+const shapeIsRed = partialRight(shapeIsColor, [COLORS.RED]);
+const shapeIsBlue = partialRight(shapeIsColor, [COLORS.BLUE]);
+const shapeIsOrange = partialRight(shapeIsColor, [COLORS.ORANGE]);
+
+const propsAreEqual = (obj, prop1, prop2) => equals(prop(prop1, obj), prop(prop2, obj));
+
+const getColors = (object) => {
+  const objKeys = keys(object);
+  return map(shape => prop(shape, object), objKeys);
+}
+
+const allIsColor = (color, object) => {
+  const colors = getColors(object);
+  const isColor = equals(color);
+  return all(isColor)(colors);
+}
+
+const countColor = (color, object) => {
+  const colors = getColors(object);
+  const filteredColors = filter(col => col === color, colors);
+  return length(filteredColors);
+}
+
+// 1. Красная звезда, зеленый квадрат, все остальные белые.
+export const validateFieldN1 = allPass([
+    shapeIsRed(SHAPES.STAR),
+    shapeIsGreen(SHAPES.SQUARE),
+    shapeIsWhite(SHAPES.TRIANGLE),
+    shapeIsWhite(SHAPES.CIRCLE)
+  ]);
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = (object) => gte(countColor(COLORS.GREEN, object), 2);
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = (object) => equals(
+  countColor(COLORS.RED, object), countColor(COLORS.BLUE, object)
+);
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = allPass([
+  shapeIsRed(SHAPES.STAR),
+  shapeIsOrange(SHAPES.SQUARE),
+  shapeIsBlue(SHAPES.CIRCLE)
+]);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+
+// меня слегка душит жаба итерироваться по ключам входного объекта все 5 раз, но здесь объем данных такой маленький, что, наверно, можно
+const countMaxColor = (object) => {
+  return reduce(max, -Infinity, values(map(color => countColor(color, object), COLORS)));
+}
+
+export const validateFieldN5 = (object) => all(val => val === true, [
+  lte(countColor(COLORS.WHITE, object), 1),
+  gte(countMaxColor(object), 3)
+]);
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = (object) => all(
+  val => val === true,
+  [
+    shapeIsGreen(SHAPES.TRIANGLE)(object),
+    equals(countColor(COLORS.GREEN, object), 2),
+    equals(countColor(COLORS.RED, object), 1),
+  ]
+);
+
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = (object) => allIsColor(COLORS.ORANGE, object);
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = complement(
+  anyPass([shapeIsWhite(SHAPES.STAR), shapeIsRed(SHAPES.STAR)])
+);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = (object) => allIsColor(COLORS.GREEN, object);
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+const triangleEqualsSquare = partialRight(propsAreEqual, [SHAPES.TRIANGLE, SHAPES.SQUARE]);
+
+export const validateFieldN10 = allPass([
+  triangleEqualsSquare,
+  complement(anyPass([shapeIsWhite(SHAPES.TRIANGLE), shapeIsWhite(SHAPES.SQUARE)]))
+]);
